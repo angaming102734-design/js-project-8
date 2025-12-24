75f47b486e81c0e906d4d9dafe9837c323231c30
ather ali <a.ngaming102734@gmail.com>
2025-12-25 00:21:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+var arr1 = ['SSC','HSC','BDS','BS','BCOM','MS','M.Phil','PHD'];
+ 
+document.write(arr1+'<br>');
+
+console.log(arr1);
+
+var studentName =('<h1>Top movies name 2025</h1>'+'<br>');
+
+document.write(studentName)
+
+var moviesName = ['Dhanrundher'+'<br>','RRR'+'<br>','Tiger3'+'<br>','Herapehri']
+
+document.write(moviesName+'<br>');
+
+console.log(moviesName.length);
+
+var cars = ('<h1>Favorite Cars</h1>')
+
+document.write(cars);
+
+var favoriteCars = ['Audi'+'<br>','BMW M5'+'<br>','Ford'+'<br>','Lamborghini 63']
+
+document.write(favoriteCars)
+
+console.log(favoriteCars.length);
+
+document.write('<br>')
+
+document.write('<br>')
+
+var Boot =["<h1>Foot Ball Name</h1>"]
+
+console.log(Boot)
+
+document.write(Boot);
+
+document.write('<br>')
+
+var Score =('<b>CR7 Total score Enter ok Thank you</b>'+' ')
+
+
+var Namescore =Number(prompt("CR7 Score"));
+
+document.write(Score+' ');
+
+document.write(Namescore);
+
+console.log(Score+Namescore)
+
+document.write('<br>')
+
+var score =('<b>Messi Total score Enter ok Thank you</b>'+' ')
+
+var threesScore =Number(prompt('Messi Score'));
+
+document.write(score+' ')
+
+document.write(threesScore)
+
+console.log(score+threesScore);
+
+var cit =['Karachi','Lahore','Multan','Islamabad']
+
+console.log(cit);
+cit.shift(2,3,);
+
+var City=['Karachi','Multan','Lahore','Peshawer','Islamabad'];
+
+console.log('Before',City)
+
+var After=['Karachi','Multan','Lahore','Peshawer','Islamabad']
+
+After.shift(1,4);
+
+After.pop();
+
+console.log('After',After);
+
+document.write('<br>')
+
+// var arr3 =['<h1></h1>')
+
+var arr3=['1','2','3' ];
+
+document.write(arr3)
